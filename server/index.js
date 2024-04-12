5e9feebe5cf748f0b4bf41e9cb4efcbaa56cee99
tongueincheek <140826014+tongueincheek@users.noreply.github.com>
2024-04-12 03:32:40 +0000

    Server: update API things

--- server/index.js
+++ server/index.js
@@ -5,38 +5,63 @@
 
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 9000
 import * as databaseQueries from './databaseQueries.js'
 
 app.get('/', (req, res) => {
-  res.sendStatus(200)
+  res.send(`
+    <div>
+      <p>Welcome to ticketing API</p>
+      <p>GET /tickets</p>
+      <p>GET /tickets/open</p>
+      <p>GET /tickets/closed</p>
+      <p>GET /tickets/byuser?userid=1</p>
+      <p>GET /tickets/byseverity?severity=1</p>
+    </div>
+    `)
 })
 
 app.get('/tickets', (req, res) => {
-    // GET /tickets
-    databaseQueries.getAllTickets().then(tickets => res.send(tickets))
+  // GET /tickets
+  databaseQueries.getAllTickets().then(tickets => res.send(tickets))
 })
 
 app.get('/tickets/open', (req, res) => {
-    // GET /tickets/open
-    databaseQueries.getOpenTickets().then(tickets => res.send(tickets))
+  // GET /tickets/open
+  databaseQueries.getOpenTickets().then(tickets => {
+    console.log(`${tickets.length} tickets are open`)
+    res.send(tickets)
+    }
+  )
 })
 
 app.get('/tickets/closed', (req, res) => {
   // GET /tickets/closed
-  databaseQueries.getClosedTickets().then(tickets => res.send(tickets))
+  databaseQueries.getClosedTickets().then(tickets => {
+    console.log(`${tickets.length} tickets are closed`)
+    res.send(tickets)
+    }
+  )
 })
 
 app.get('/tickets/byuser', (req, res) => {
   // GET /tickets/byuser?userid=1
   const { userid } = req.query
-  databaseQueries.getTicketsByUserId(userid).then(tickets => res.send(tickets))
+  databaseQueries.getTicketsByUserId(userid).then(tickets => {
+    console.log(`User ${userid} has ${tickets.length} tickets`)
+    res.send(tickets)
+    }
+  )
 })
 
 app.get('/tickets/byseverity', (req, res) => {
   // GET /tickets/byseverity?severity=1
   const { severity } = req.query
-  databaseQueries.getTicketsBySeverity(severity).then(tickets => res.send(tickets))
+  databaseQueries.getTicketsBySeverity(severity).then(tickets => {
+      console.log(`${tickets.length} tickets are considered ${severity}`)
+      res.send(tickets)
+    }
+  )
 })
 
 app.listen(port, () => {
